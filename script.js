e3a26b259d693ff6b103907061269c0a26b6443a
Renelle <titingrenelle@gmail.com>
2017-03-01 12:49:44 +0800

    Improve the game by deleting the alert function

--- script.js
+++ script.js
@@ -4,15 +4,13 @@ var guessNumber = parseInt(prompt("I'm thinking of a number between 1 and 6. Wha
 if (guessNumber === randomNumber) {
   correctGuess = true;
 } else if (guessNumber > randomNumber) {
-  alert("The secret number is lower than " + guessNumber);
-  guessNumber = parseInt(prompt("I'm thinking of a number between 1 and 6. What is it?"));
-    if (guessNumber === randomNumber) {
+  var secondGuess = parseInt(prompt("Try again. The number I'm thinking is lower than" + guessNumber));
+    if (secondGuess === randomNumber) {
       correctGuess = true;
     }
 } else {
-  alert("The secret number is higher than " + guessNumber);
-  guessNumber = parseInt(prompt("I'm thinking of a number between 1 and 6. What is it?"));
-    if (guessNumber === randomNumber) {
+  var secondGuess = parseInt(prompt("Try again. The number I'm thinking is higher than" + guessNumber));
+    if (secondGuess === randomNumber) {
       correctGuess = true;
     }
 } 
